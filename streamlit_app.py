3d6819b1d54cc70a77a4a71fe4a5bb244a503b3e
a01798 <andreas-lokken.heggelund@norsk-tipping.no>
2024-07-08 09:39:04 +0200

    added nutritional values to ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,6 @@ st.write(
     """
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -31,20 +27,20 @@ if ingredient_list:
 
     for ingredient in ingredient_list:
         ingredient_string += ingredient + ' '
-    
+        st.subheader(ingredient + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
     #st.write(ingredient_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredient_string + """', '""" + name + """')"""
-    
-    st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered, ' + name + "!", icon="✅")
 
 
 
