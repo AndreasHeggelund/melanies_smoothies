bab8b204244aa3b6c42ddc8b3f85465e36ec474e
a01798 <andreas-lokken.heggelund@norsk-tipping.no>
2024-07-08 09:33:44 +0200

    added API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title(":tropical_drink: Customize your Smoothie! :tropical_drink:")
@@ -9,6 +9,9 @@ st.write(
     """This is an app for customizing your own smoothie!
     """
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
