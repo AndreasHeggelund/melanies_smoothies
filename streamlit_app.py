43001dddcb32edfc06578e956dc59158786c08b9
a01798 <andreas-lokken.heggelund@norsk-tipping.no>
2024-07-08 09:35:10 +0200

    test dataframe conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ st.write(
 )
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
